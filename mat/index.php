e7644022745cf97f0d6aae7ebe4929a02680a637
buralien <github@lty.cz>
2017-10-22 19:24:52 +0200

    Added caching prevention headers

--- mat/index.php
+++ mat/index.php
@@ -7,6 +7,11 @@ if (isset($_GET['startover'])) {
   header('Location: ?');
   die();
 }
+header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+header("Pragma: no-cache"); // HTTP/1.0
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 
 require_once 'include/level.class.php';
 require_once 'HTML/Page2.php';

de6ca8470062aec319c2388ff8eb1ef6b57773a2
buralien <github@lty.cz>
2017-11-28 05:46:55 +0100

    Add link to Github commit page

--- mat/include/footer.php
+++ mat/include/footer.php
@@ -1,7 +1,8 @@
 <?php
 
 $html->addBodyContent('<div class="footer">');
-$html->addBodyContent('<p>Commit: '. shell_exec('cd '. realpath(dirname(__FILE__)). '/../../ && git log --pretty="%h" -n1 HEAD'). '</p>');
+$githash = shell_exec('cd '. realpath(dirname(__FILE__)). '/../../ && git log --pretty="%H" -n1 HEAD');
+$html->addBodyContent('<p>Commit: <a href="https://github.com/buralien/mat/commit/'. $githash. '">'. substr($githash, 0, 7). '</a></p>');
 $html->addBodyContent('<p>MAT info a licence: <a href="https://github.com/buralien/mat">GitHub</a></p>');
 $html->addBodyContent('<div><a href="https://responsivevoice.org">ResponsiveVoice-NonCommercial</a> licensed under <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/">CC BY-NC-ND 4.0</a></div>');
 $html->addBodyContent('<p>P&rcaron;&iacute;sp&ecaron;vek na provoz: <a href="https://paypal.me/buralien">PayPal</a></p>');

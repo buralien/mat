42971a2dfb4149068075c9e429fccb7245d6f3e1
buralien <github@lty.cz>
2017-11-28 05:35:05 +0100

    IMG to text for better responsivenes

--- mat/include/footer.php
+++ mat/include/footer.php
@@ -3,7 +3,7 @@
 $html->addBodyContent('<div class="footer">');
 $html->addBodyContent('<p>Commit: '. shell_exec('cd '. realpath(dirname(__FILE__)). '/../../ && git log --pretty="%h" -n1 HEAD'). '</p>');
 $html->addBodyContent('<p>MAT info a licence: <a href="https://github.com/buralien/mat">GitHub</a></p>');
-$html->addBodyContent('<div><a href="https://responsivevoice.org">ResponsiveVoice-NonCommercial</a> licensed under <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/"><img title="ResponsiveVoice Text To Speech" src="https://responsivevoice.org/wp-content/uploads/2014/08/95x15.png" alt="95x15" width="95" height="15" /></a></div>');
+$html->addBodyContent('<div><a href="https://responsivevoice.org">ResponsiveVoice-NonCommercial</a> licensed under <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/">CC BY-NC-ND 4.0</a></div>');
 $html->addBodyContent('<p>P&rcaron;&iacute;sp&ecaron;vek na provoz: <a href="https://paypal.me/buralien">PayPal</a></p>');
 $html->addBodyContent('</div>');
 

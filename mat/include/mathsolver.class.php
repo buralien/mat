571cc2a2367b0a9448862399d1377e42c86b4840
buralien <github@lty.cz>
2017-12-26 22:17:14 +0100

    Better input validation for Mathsolver

--- mat/include/mathsolver.class.php
+++ mat/include/mathsolver.class.php
@@ -20,18 +20,26 @@ class FormulaSolver {
   public static $translate_operators = array('+' => OP_PLUS, '-' => OP_MINUS, 'x' => OP_KRAT, '*' => OP_KRAT, ':' => OP_DELENO, '/' => OP_DELENO);
 
   /**
-  * @param FormulaElement $element1
-  * @param FormulaElement $element2
-  * @param OperatorElement $operator
+  * @param mixed $element1 Either a FormulaElement or a number
+  * @param mixed $operator Either a OperatorElement or a number or a string
+  * @param mixed $element2 Either a FormulaElement or a number
   * @return integer The solution
   */
   public static function solve($element1, $operator, $element2) {
-    if (is_a($element1, 'FormulaElement')) $element1 = $element1->getValue();
-    if (!is_numeric($element1)) throw new Exception('Invalid input 1');
-    if (is_a($element2, 'FormulaElement')) $element2 = $element2->getValue();
-    if (!is_numeric($element2)) throw new Exception('Invalid input 2');
-    if (is_a($operator, 'OperatorElement')) $operator = $operator->getMath();
-    if (!in_array($operator, array_keys(static::$allowed_operators))) throw new Exception('Invalid operator input');
+    if (is_subclass_of($element1, 'FormulaElement')) $element1 = $element1->getValue();
+    if (!is_int($element1)) throw new Exception('Invalid input 1');
+
+    if (is_subclass_of($element2, 'FormulaElement')) $element2 = $element2->getValue();
+    if (!is_int($element2)) throw new Exception('Invalid input 2');
+
+    if (is_string($operator)) {
+      if (in_array($operator, static::$translate_operators)) {
+        $operator = new OperatorElement(static::$translate_operators[$operator]);
+      } else throw new Exception('Invalid string operator input');
+    } elseif (is_int($operator)) $operator = new OperatorElement($operator);
+    elseif (!is_a($operator, 'OperatorElement')) throw new Exception('Invalid operator input');
+    $operator = $operator->getMath();
+
     $expr = $element1 . $operator . $element2;
     return eval('return '. $expr. ';');
   }
@@ -42,12 +50,19 @@ class FormulaSolver {
   * @return integer The solution
   */
   public static function multisolve($elements, $operators) {
+    if (!is_array($elements) || !is_array($operators)) throw new Exception('Invalid inputs');
     if(count($elements) < 1 || count($operators) < 1) throw new Exception('No input');
     if((count($elements) - 1) != count($operators)) throw new Exception('Invalid input combination');
-    $expr = array_shift($elements)->getValue();
-    while(count($operators) > 0) {
-      $expr .= array_shift($operators)->getMath();
-      $expr .= array_shift($elements)->getValue();
+    $el = array_shift($elements);
+    if (!is_subclass_of($el, 'FormulaElement')) throw new Exception('Invalid element input type');
+    $expr = $el->toStr();
+    while(count($operators) > 0 && count($elements) > 0) {
+      $op = array_shift($operators);
+      if (!is_a($op, 'OperatorElement')) throw new Exception('Invalid operator input type');
+      $expr .= $op->getMath();
+      $el = array_shift($elements);
+      if (!is_subclass_of($el, 'FormulaElement')) throw new Exception('Invalid element input type');
+      $expr .= $el->toStr();
     }
     return eval('return '. $expr. ';');
   }
